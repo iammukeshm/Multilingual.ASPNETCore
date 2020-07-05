befd1d0e7211ba265ebb1b6da8e88924c6ab6206
Mukesh Murugan <iammukeshm@gmail.com>
2020-07-05 16:35:51 +0530

    Add project files.

---
+++ Multilingual.ASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Multilingual.ASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
